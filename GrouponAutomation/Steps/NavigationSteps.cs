4050d538dff2998d5bbce43b4f725e44d0322ca6
sgnanas <sgnanas@transunion.com>
2017-08-01 14:02:48 -0600

    deleted spaces

--- GrouponAutomation/Steps/NavigationSteps.cs
+++ GrouponAutomation/Steps/NavigationSteps.cs
@@ -21,9 +21,6 @@ namespace GrouponAutomation.Steps
         IWebDriver driver = ScenarioContext.Current.Get<IWebDriver>("IWebDriver");
         
 
-
-
-
         [Given(@"I go to the Groupon HomePage")]
         public void GivenIGoToTheGrouponHomePage()
         {
